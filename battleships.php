4bd2d0358a06b6f790420d92fb2ec144f8e78c89
JordanKyr <jordankiriazidis@hotmail.com>
2023-12-23 19:45:47 +0200

    Update battleships.php

--- battleships.php
+++ battleships.php
@@ -1,7 +1,7 @@
 <?php
 $method = $_SERVER['REQUEST_METHOD'];
-//$request = explode('/', trim($_SERVER['PATH_INFO'],'/’));
- $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/’)); 
+//$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+ $request = explode('/', trim($_SERVER['SCRIPT_NAME'],'/')); 
 // Σε περίπτωση που τρέχουμε php–S 
 
 $input = json_decode(file_get_contents('php://input'),true);
